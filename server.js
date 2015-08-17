25a0c4c2d2a511f3e13c313a5058726b4d16a253
Kaya Kim <9474ing.rain@gmail.com>
2015-08-17 14:43:25 -0700

    setting up

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var path= require("path");
+
+require('./server/config/mongoose.js');
+require('./server/config/routes.js')(app);
+
+app.use(express.static(path.join(__dirname, './client')));
+
+app.listen(8000, function() {
+	console.log('listening on port 8000');
+});
+
